a965820c4941c19ad589ca6f0284e1e758c36cda
Filipe Cajado <filipe.cajado@wedotec.com.br>
2023-05-24 14:49:42 -0300

    component search filter

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subject } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
 import { PhotoService } from '../photo/photo.service';
 
 @Component({
@@ -9,10 +7,9 @@ import { PhotoService } from '../photo/photo.service';
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.scss']
 })
-export class PhotoListComponent implements OnInit, OnDestroy {
+export class PhotoListComponent implements OnInit {
   photos: any[] = [];
   filter: string = '';
-  debounce: Subject<string> = new Subject<string>();
   hasMore: boolean = true;
   currentPage: number = 1;
   userName: string = '';
@@ -26,18 +23,14 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.userName = this.activatedRoute.snapshot.params['userName'];
     this.photos = this.activatedRoute.snapshot.data['photos'];
-    this.debounce
-      .pipe(debounceTime(300))
-      .subscribe(filter => this.filter = filter);
   }
 
-  ngOnDestroy(): void {
-    this.debounce.unsubscribe();
-  }
+  
 
   load(){
     this.photoService.listFromUserPagineted(this.userName, ++this.currentPage)
     .subscribe(photos => {
+      this.filter = '';
       this.photos = this.photos.concat(photos);
       if(!photos.length){
         this.hasMore = false
