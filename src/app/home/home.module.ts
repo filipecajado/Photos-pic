0024cac61616ef3aba353c41f2aaa2901a0aee51
Filipe Cajado <filipecajado@ucl.br>
2023-05-25 11:22:19 -0300

    Form Validator

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from "@angular/core";
 import { SigninComponent } from "./signin/signin.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+import { VMessageModule } from "../shared/components/vmessage/vmessage.module";
 
 @NgModule({
-    declarations: [ SigninComponent]
+    declarations: [ SigninComponent],
+    imports: [ ReactiveFormsModule, CommonModule, VMessageModule]
 })
 export class HomeModule{
 
--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,8 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
     templateUrl: './signin.component.html'
 })
-export class SigninComponent{
+export class SigninComponent implements OnInit{
 
+    loginForm!: FormGroup;
+
+
+    constructor(private formBuilder: FormBuilder){}
+
+    ngOnInit(): void {
+        this.loginForm = this.formBuilder.group({
+            userName: ['', Validators.required],
+            password: ['', Validators.required]
+        })
+    }
 }
