0a0aeb3437ee7c17cd8bddf183490f5f2a9e2d17
Filipe Cajado <filipecajado@ucl.br>
2023-06-27 10:17:11 -0300

    add comments and get comments

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -2,21 +2,25 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { PhotoService } from "../photo/photo.service";
 import { Photo } from "../photo/photo";
+import { Observable } from "rxjs";
+import { PhotoComment } from "../photo/photo-comment";
 
 @Component({
     templateUrl: './photo-details.component.html',
+    styleUrls: ['./photo-details.scss']
 })
 export class PhotoDetailsComponent implements OnInit{
 
-    photo!: Photo;
+    photo$!: Observable<Photo>;
+    comments$!: Observable<PhotoComment[]>;
 
     constructor(private route: ActivatedRoute,
             private photoService: PhotoService){}
 
     ngOnInit(): void {
-        const id = this.route.snapshot.params['photoId'];
-        this.photoService.findById(id).subscribe(photo => this.photo = photo)
-        console.log(id);
+        const id = this.route.snapshot.params['photoId']
+        this.photo$ = this.photoService.findById(id)
+        this.comments$ = this.photoService.getComments(id)
     }
 
 }
